818dd67efac635e6e5e22900cae107f8dc700f77
kuwiki <siketaiscience@gmail.com>
2020-06-27 23:37:09 +0900

    Update nuxt.config.js
    色 設定方法 一例

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+import colors from 'vuetify/es5/util/colors'
 
 const config = {
   mode: 'universal',
@@ -57,6 +58,31 @@ const config = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+  },
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      themes: {
+        light: {
+          primary: '#1976D2',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+        },
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
   },
   auth: {
   },
