c660cdd61f7d3066484caba48b0b6557dbf7a0ca
kuwiki <siketaiscience@gmail.com>
2020-06-28 21:45:19 +0900

    Create signin view by Vuetify

---
+++ pages/signin_new.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <v-container class="fill-height">
+      <v-card outlined class="mx-auto" width="300px">
+        <v-card-text class="pa-6">
+          <v-form class="mx-6 my-2">
+            <v-text-field v-model="email" :rules="emailRules" label="email" autofocus required >
+            </v-text-field>
+            <v-text-field v-model="password" :rules="passwordRules" :type="'password'" label="password" reqired >
+            </v-text-field>
+          </v-form>
+          <v-card-actions class="mx-3 my-2">
+            <v-btn color="primary" depressed block>ログイン</v-btn>
+          </v-card-actions>
+        </v-card-text>
+      </v-card>
+    </v-container>
+
+  </v-app>
+</div>
+</template>
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    email: '',
+    password: '',
+    // emailRules: [
+    //   v => /^.+@st.kyoto-u.ac.jp$/.test(v) || '学生用メール @st.kyoto-u.ac.jp',
+    // ],
+  })
+}
+</script>
