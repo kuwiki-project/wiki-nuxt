32ac7a58a101b72651443fa1a75abdcbb665adb4
kuwiki <siketaiscience@gmail.com>
2020-07-15 21:05:19 +0900

    Create original top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,29 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        wiki-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My classy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<v-app id="index">
+  <v-container class="fill-height">
+    <v-row>
+      <span class='text-h1 mx-auto'>京大wiki</span>
+    </v-row>
+    <v-row>
+      <span class='mx-auto'><v-text-field solo rounded></v-text-field></span>
+    </v-row>
+    <v-row align='center' justify='center'>
+      <v-card outlined>
+      <v-card-title>ダッシュボード</v-card-title>
+    </v-card>
+      <v-card outlined>
+      <v-card-title>お問い合わせ</v-card-title>
+    </v-card>
+      <v-card outlined>
+      <v-card-title></v-card-title>
+    </v-card>
+    </v-row>
+  </v-container>
+</v-app>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
-
-export default Vue.extend({
-  components: {
-    Logo
-  }
-})
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+/deep/ .v-text-field{
+      width: 400px;
 }
 </style>
