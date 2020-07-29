37fa089d5f48331f9a93183dc2f7ae27c1206a6d
kuwiki <siketaiscience@gmail.com>
2020-07-29 19:40:39 +0900

    Create indexList component

---
+++ components/indexList.vue
@@ -0,0 +1,29 @@
+<template>
+<v-list nav dense rounded>
+<v-list-item
+  v-for="item in items"
+  :key="item.text"
+  link >
+    <v-list-item-icon>
+      <v-icon>{{ item.icon }}</v-icon>
+    </v-list-item-icon>
+  <v-list-item-content>
+    <v-list-item-title>
+      {{ item.text }}
+    </v-list-item-title>
+  </v-list-item-content>
+</v-list-item>
+</v-list>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { icon: 'mdi-file-outline', text: '過去問検索' },
+      { icon: 'mdi-video-account', text: '教員検索' },
+      { icon: 'mdi-file-upload-outline', text: '過去問提供' },
+    ]
+    })
+  }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,37 @@
 <template>
-<div>
-<navi></navi>
-  <nuxt />
-</div>
+<v-app id='navi'>
+  <v-navigation-drawer class="teal" permanent floating app dark mini-variant>
+    <v-list nav dense rounded>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-penguin</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>京大wiki</v-list-item-title>
+      </v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+    <indexList></indexList>
+    <v-divider></v-divider>
+
+    <v-list nav dense rounded>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-emoticon-happy-outline</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>アカウント</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <v-main>
+    <nuxt />
+  </v-main>
+</v-app>
 </template>
 <script>
-import navi from "@/components/navi.vue";
+import indexList from "@/components/indexList.vue";
 export default {
   components: {
-    navi
+    indexList
   }
 }
 </script>
