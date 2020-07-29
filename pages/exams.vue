66f08a8109f29b189077f3dd88d8ad35091dd334
kuwiki <siketaiscience@gmail.com>
2020-07-30 02:23:49 +0900

    Create exams.vue

---
+++ pages/exams.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app id='index'>
+    <v-container class="mx-auto">
+      <v-row align='center' justify='center'>
+        <span id='title'>過去問検索</span>
+      </v-row>
+      <v-row align='center' justify='center'>
+        <searchForm></searchForm>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
