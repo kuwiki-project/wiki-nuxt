1f9d66decb437f091042c136f1dc1f573c47f8a6
kuwiki <siketaiscience@gmail.com>
2020-08-05 15:56:47 +0900

    Remove SearchList plugin
    使わなさそう

--- plugins/components.js
+++ plugins/components.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import NaviItems from "@/components/plugins/NaviItems.vue";
 import SearchTitle from "@/components/plugins/SearchTitle.vue";
 import SearchForm from "@/components/plugins/SearchForm.vue";
-import SearchList from "@/components/plugins/SearchList.vue";
 import KupediaLogo from "@/components/plugins/KupediaLogo.vue";
 Vue.component('NaviItems', NaviItems)
 Vue.component('SearchTitle', SearchTitle)
 Vue.component('SearchForm', SearchForm)
-Vue.component('SearchList', SearchList)
 Vue.component('KupediaLogo', KupediaLogo)
