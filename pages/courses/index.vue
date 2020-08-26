4af07268365039f49ac8e973ddff694dc7d43a41
kuwiki <siketaiscience@gmail.com>
2020-08-26 15:17:39 +0900

    Get course index json from api

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -7,16 +7,14 @@
 
 <script>
 import axios from 'axios';
-import courses from '@/assets/json/courses.json'
 export default {
   data: () => ({
     title: '科目検索',
-    // courses: courses
   }),
   async asyncData() {
-    // const baseURL = process.env.WIKI_API_URL + '/courses'
-    // const { data } = await axios.get(baseURL)
-    return { courses: courses }
+    const baseURL = process.env.WIKI_API_URL + '/courses'
+    const { data } = await axios.get(baseURL)
+    return { courses: data }
   },
 }
 </script>
