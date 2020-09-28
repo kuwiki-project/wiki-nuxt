1649657706314004621dec0711b83f8a82591948
kuwiki <siketaiscience@gmail.com>
2020-09-28 21:59:42 +0900

    Delete Naviindex & Naviitems

--- plugins/components.js
+++ plugins/components.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
-import NaviItems from "@/components/plugins/NaviItems.vue";
 import SearchTitle from "@/components/plugins/SearchTitle.vue";
 import SearchForm from "@/components/plugins/SearchForm.vue";
 import KupediaLogo from "@/components/plugins/KupediaLogo.vue";
-Vue.component('NaviItems', NaviItems)
 Vue.component('SearchTitle', SearchTitle)
 Vue.component('SearchForm', SearchForm)
 Vue.component('KupediaLogo', KupediaLogo)
