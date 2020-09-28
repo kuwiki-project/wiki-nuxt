600de55100687e6d720aec8d8d99480b42321df4
kuwiki <siketaiscience@gmail.com>
2020-09-28 22:01:51 +0900

    Update layout/default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,54 @@
 <template>
-<v-app id='navi'>
-  <v-navigation-drawer class="teal" permanent floating app dark mini-variant>
-    <NaviItems :items="topItem"></NaviItems>
-    <v-divider></v-divider>
-    <NaviIndex></NaviIndex>
-    <v-divider></v-divider>
-    <NaviItems :items="accountItem"></NaviItems>
-  </v-navigation-drawer>
-  <v-main>
-    <nuxt />
-  </v-main>
+  <v-app id='navi'>
+  <v-app-bar flat dense dark class="indigo accent-1">
+    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    <v-toolbar-title>京大wiki</v-toolbar-title>
+  </v-app-bar>
+  <v-navigation-drawer v-model="drawer" absolute temporary>
+    <v-list nav dense>
+      <v-list-item-group v-model="group">
+        <v-list-item link v-for="item in items" :key="item.text" :to="item.link" >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+         <v-list-item-content>
+           <v-list-item-title>
+            {{ item.text }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
+</v-navigation-drawer>
+  <nuxt />
 </v-app>
 </template>
 <script>
-import NaviIndex from "@/components/NaviIndex.vue";
 export default {
-  components: {
-    NaviIndex,
-  },
-  data: () => ({
-  topItem: [
-    { icon: 'mdi-led-variant-outline', text: '京大wiki' , link:'/'},
-  ],
-  accountItem: [
-    { icon: 'mdi-emoticon-happy-outline', text: 'アカウント' , link:'/signup'},
-  ],
-  })
-}
+    data: () => ({
+      drawer: false,
+      group: null,
+      items: [{
+          icon: 'mdi-home-variant-outline',
+          text: 'ホーム',
+          link: '/'
+        },
+        {
+          icon: 'mdi-file-search-outline',
+          text: '科目検索',
+          link: '/courses'
+        },
+        {
+          icon: 'mdi-email-outline',
+          text: '問い合わせ',
+          link: ''
+        },
+        {
+          icon: 'mdi-account',
+          text: 'サインイン',
+          link: '/signin'
+        }
+      ]
+    }),
+  }
 </script>
