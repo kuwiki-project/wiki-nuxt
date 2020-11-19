5506cca76d6831ead70e0817c240a559d54c970d
kuwiki <siketaiscience@gmail.com>
2020-11-19 23:22:49 +0900

    Create favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ const config = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/kiwi.svg' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Noto+Sans&family=Noto+Sans+JP:wght@400;700&display=swap'
--- pages/contact.vue
+++ pages/contact.vue
@@ -7,10 +7,27 @@
           問い合わせ
         </h2>
         <p>
-          何か要望や問題がありましたら以下のメールアドレスまたはTwitterまでご連絡ください
+          またその他何かご要望や連絡がありましたら以下のメールアドレスまたはTwitterまでご連絡ください
         </p>
+        <p> メールアドレス: kuwiki99   (a) gmail.com</p>
+        <p> Twitter: ku_wiki </p>
       </v-card-text>
     </v-card>
   </v-container>
 </v-app>
 </template>
+<style scope>
+.termCard{
+  margin: 10pt;
+  padding: 30pt;
+}
+p.zenbun{
+  margin:0pt 0pt 20pt;
+}
+h1{
+  margin: 20pt 0pt 10pt;
+}
+h2{
+  margin: 20pt 0pt 10pt;
+}
+</style>
