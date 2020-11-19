8c81d79167cf5ed289b970deaff0acbd0bd88c65
kuwiki <siketaiscience@gmail.com>
2020-11-19 23:30:05 +0900

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id='navi'>
+<v-app id='navi'>
   <v-app-bar app dense dark flat class="primary">
     <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
     <v-toolbar-title>
@@ -11,53 +11,62 @@
   <v-navigation-drawer v-model="drawer" app>
     <v-list dense>
       <v-list-item-group v-model="group">
-        <v-list-item link v-for="item in items" :key="item.text" :to="item.link" >
+        <v-list-item link v-for="item in items" :key="item.text" :to="item.link">
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
-         <v-list-item-content>
-           <v-list-item-title>
-            {{ item.text }}
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list-item-group>
-  </v-list>
-</v-navigation-drawer>
-<v-main>
-  <nuxt />
-</v-main>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+  <v-main>
+    <nuxt />
+  </v-main>
 </v-app>
 </template>
 <script>
 export default {
-    data: () => ({
-      drawer: false,
-      group: null,
-      items: [{
-          icon: 'mdi-home-variant-outline',
-          text: 'ホーム',
-          link: '/'
-        },
-        {
-          icon: 'mdi-file-search-outline',
-          text: '科目検索',
-          link: '/courses'
-        },
-        {
-          icon: 'mdi-help-circle-outline',
-          text: '京大wikiとは',
-          link: '/kuwikiis'
-        },
-        {
-          icon: 'mdi-email-outline',
-          text: '問い合わせ',
-          link: '/contact'
-        },
-
-      ]
-    }),
-  }
+  data: () => ({
+    drawer: false,
+    group: null,
+    items: [{
+        icon: 'mdi-home',
+        text: 'ホーム',
+        link: '/'
+      },
+      {
+        icon: 'mdi-file-search-outline',
+        text: '科目検索',
+        link: '/courses'
+      },
+      {
+        icon: 'mdi-file-upload-outline',
+        text: '過去問提供',
+        link: '/volunteer'
+      },
+      {
+        icon: 'mdi-help-circle-outline',
+        text: '京大wikiとは',
+        link: '/profile'
+      },
+      {
+        icon: 'mdi-email-outline',
+        text: '問い合わせ',
+        link: '/contact'
+      },
+      {
+        icon: 'mdi-account',
+        text: 'ログイン',
+        link: '/signin'
+      }
+    ]
+  }),
+}
 </script>
 <style scoped>
 #title {
