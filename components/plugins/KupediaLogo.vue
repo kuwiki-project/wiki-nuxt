ef001a4aa05b291f4de3e5ff2daed7e538aa7618
kuwiki <siketaiscience@gmail.com>
2020-12-10 21:54:14 +0900

    Update KupediaLogo.vue
    京大wikiの文字を抜いた

--- components/plugins/KupediaLogo.vue
+++ components/plugins/KupediaLogo.vue
@@ -3,16 +3,5 @@
   <v-row justify='center' align='start'>
       <img src="/kiwi.svg" width='200' />
   </v-row>
-  <v-row justify='center' align='start'>
-      <span id='title'>京大wiki</span>
-  </v-row>
 </v-container>
 </template>
-<style scoped>
-#title {
-  opacity: 0.8;
-  font-size: 2.0em;
-  font-weight: 700;
-  font-family: 'Noto Sans JP', sans-serif;
-}
-</style>
