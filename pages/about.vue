9d4da8dadff3dd2c69e65e678623824d0abd2e2c
kuwiki <siketaiscience@gmail.com>
2020-12-10 21:55:47 +0900

    about ページ
    途中

---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app id="terms">
+    <v-container>
+      <v-card width="600pt" class="mx-auto termCard">
+        <v-card-text class="mx-auto" width="600pt">
+          <h2>京大wikiとは</h2>
+          <p>
+          学内資料を収集・整理・公開するプロジェクト
+        </p>
+          <p>
+            京大wikiは京大生有志によって運営されているサイトです．
+            上回生から受け継いだ過去問を公開することを主な目的として2018年度理学部生を中心に発足しました．
+          </p>
+
+          <p />
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+<style scope>
+.termCard {
+  margin: 10pt;
+  padding: 30pt;
+}
+
+p.zenbun {
+  margin: 0pt 0pt 20pt;
+}
+
+h1 {
+  margin: 20pt 0pt 10pt;
+}
+
+h2 {
+  margin: 20pt 0pt 10pt;
+}
+</style>
