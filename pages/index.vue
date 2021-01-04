414673b9bc2bd4124c6d7912b7ef64875dbac3ce
kuwiki <siketaiscience@gmail.com>
2021-01-05 02:30:21 +0900

    Update index.vue
    デザイン変更

--- pages/index.vue
+++ pages/index.vue
@@ -4,164 +4,223 @@
     <v-container>
       <KupediaLogo />
       <v-row class='justify-center' id='ku-wiki-is'>
-        <div id='kuwiki-is'><span class='hutoji daiji'>京大wiki</span> とは<br />
-          学内資料を<span class='hutoji'>収集・整理・公開・保存</span>するプロジェクト<br />
+        <div id='kuwiki-is'>
+          <span class='hutoji daiji'>京大wiki</span>
+          とは<br>
+          学内資料を<br>
+          <span class='hutoji'>収集・整理・公開</span>するプロジェクト
+          <!-- <v-icon>mdi-file-upload-outline</v-icon>収集
+           <v-icon>mdi-file-cabinet</v-icon>整理
+            <v-icon>mdi-web</v-icon>公開 -->
         </div>
-      </v-row>
-      <v-row justify='center' class="my-7">
-        <v-col cols="2" class="justify-center">
-          <v-avatar color='primary' size='84'>
-            <v-icon x-large dark>mdi-file-upload-outline</v-icon>
-          </v-avatar>
-          <p>収集</p>
-        </v-col>
-
-        <v-col cols="2" class='justify-center'>
-          <v-avatar color='primary' size='84'>
-            <v-icon x-large dark>mdi-file-cabinet</v-icon>
-          </v-avatar>
-          <p>整理</p>
-        </v-col>
-
-        <v-col cols="2" class="justify-center">
-          <v-avatar color='primary' size='84'>
-            <v-icon x-large dark>mdi-web</v-icon>
-          </v-avatar>
-          <p>公開</p>
-        </v-col>
       </v-row>
     </v-container>
 
     <v-container>
       <v-row>
         <v-col cols='12'>
-          <h2>京大wikiでできること</h2>
-          <h3>サイト閲覧</h3>過去問や新入生向け記事を閲覧できます
-          <h3>過去問提供</h3>過去問を閲覧する方はご協力お願いします．一科目から提供できます
-          <h3>記事投稿</h3>詳しくはこちら
-          <h3>API利用</h3>開発者向けにAPIの利用方法を公開しています
-          <h3>サイト開発</h3>フロントエンド，バックエンドともにソースを公開しています
-          </ul>
+          <h3>京大wikiでできること</h3>
+          <dl>
+            <div>
+              <dt>
+                <v-icon>mdi-search-web</v-icon> サイト閲覧
+              </dt>
+              <dd>過去問やwikiを閲覧できます</dd>
+            </div>
+            <div>
+              <dt>
+                <v-icon>mdi-file-upload-outline</v-icon> 過去問提供
+              </dt>
+              <dd>一科目から提供できます．過去問をご覧になる方はご協力ください</dd>
+            </div>
+            <div>
+              <dt>
+                <v-icon color='blue'>mdi-fountain-pen</v-icon>
+                記事投稿
+              </dt>
+              <dd>wikiの記事を執筆できます</dd>
+            </div>
+            <div>
+              <dt>
+                <v-icon>mdi-code-json</v-icon> API利用
+              </dt>
+              <dd>開発者向けにAPIの利用方法を公開しています</dd>
+            </div>
+            <div>
+              <dt>
+                <v-icon>mdi-file-code-outline</v-icon> サイト開発
+              </dt>
+              <dd>フロントエンド，バックエンドともにオープンソースです</dd>
+            </div>
+          </dl>
         </v-col>
       </v-row>
-    </v-container>
-
-    <v-container>
       <v-row>
-        <v-col cols='6'>
-          <v-card>
-            <v-card-title>📝 過去問</v-card-title>
+        <v-col cols='12' sm='6'>
+          <v-card outlined>
+            <v-card-title>
+              <v-avatar size="26" class='my-1 mx-2'>
+                <v-icon>mdi-folder-clock</v-icon>
+              </v-avatar>
+              過去問
+            </v-card-title>
             <v-card-subtitle>
+              全学で過去問を共有できます．
               すべての般教科目・専門科目の試験問題およびレポート課題を取り扱っています</v-card-subtitle>
             <v-card-text>
-              <p>
-                <h3>収集</h3>
-                <div>過去を閲覧している方から回収</div>
-                <ul>
-                  <li>
-                    📧 メールで送る<nuxt-link to="/volunteer">こちら</nuxt-link>
-                  </li>
-                  <li>📂 学内回収ボックスに入れる/コロナで中断中</li>
-                  <li>🖇 連携する自治会</li>
-                </ul>
-              </p>
-              <p>
-                <h3>整理</h3>
-                有志数名で手動&自動で整理しています
-              </p>
-              <p>
-                <h3>公開</h3>
-                <nuxt-link to="/courses">こちらのページ</nuxt-link> で公開中
-              </p>
+              <v-timeline align-top dense>
+                <v-timeline-item small color='secondary'>
+                  <h4>収集</h4>
+                  <div>過去問を利用する方から集めています</div>
+                  <ul>
+                    <li>
+                      📧 メール<nuxt-link to="/volunteer">こちら</nuxt-link>
+                    </li>
+                    <li>📂 学内回収ボックス/コロナで中断中</li>
+                    <li>🖇 連携する自治会</li>
+                  </ul>
+                </v-timeline-item>
+                <v-timeline-item small>
+                  <h4>整理</h4>
+                  有志数名で手動&自動で整理しています
+                </v-timeline-item>
+                <v-timeline-item small>
+                  <h4>公開</h4>
+                  <nuxt-link to="/courses">こちらのページ</nuxt-link> で公開中
+                  ソースはこちら
+                </v-timeline-item>
+              </v-timeline>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col cols='6'>
-          <v-card>
-            <v-card-title>📕 wiki</v-card-title>
+
+        <v-col cols='12' sm='6'>
+          <v-card outlined>
+            <v-card-title>
+              <v-avatar size="26" class='my-1 mx-2'>
+                <v-icon>mdi-book-open-variant</v-icon>
+              </v-avatar> wiki
+            </v-card-title>
             <v-card-subtitle>
               京大内のさまざまなことを事典的にまとめています<br />
-              🌸 新入生必見
+              🌸新入生必見
             </v-card-subtitle>
             <v-card-text>
-              <p>
-            <h3>記事作成</h3>
-            記事を書いたり，他団体と協力して記事を書いたり，他webサイトのリンクを集めたりしています．
-            順次拡張予定です．
-            記事執筆者求 お気軽に連絡ください
-          </p>
-          <p>
-            <h3>掲載</h3>
-          </p>
-          </v-card-text>
+              <v-timeline align-top dense>
+                <v-timeline-item small color='secondary'>
+                  <h4>記事作成</h4>
+                  記事を書いたり，他webサイトのリンクを集めたりしています．順次拡張予定
+                </v-timeline-item>
+
+                <v-timeline-item small color='secondary'>
+
+                  <h4>掲載</h4>
+                  こちらのページで公開中
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
           </v-card>
         </v-col>
-        </v-row>
-        <v-col>
+        <v-col cols='12' sm='6'>
+          <v-card outlined>
+            <v-card-title>
+              <v-avatar size="26" class='my-1 mx-2'>
+                <v-icon>mdi-code-json</v-icon>
+              </v-avatar>
+              API
+            </v-card-title>
+            <v-card-subtitle>
+              京大wikiが収集したデータを利用できます<br />
+            </v-card-subtitle>
+            <v-card-text>
+              <v-timeline align-top dense>
+                <v-timeline-item small>
+                  <h4>作成</h4>
+                  Ruby on Railsで作成
+                </v-timeline-item>
+                <v-timeline-item small>
+                  <h4>利用方法</h4>
+                  READMEに掲載
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols='12' sm='6'>
           ...その他準備中
         </v-col>
-
-
-    </v-container>
-
-    <v-container>
+      </v-row>
       <v-row>
         <v-col cols='12'>
-          <h2>活動意義</h2>
-          学生の立場で，学内の資料を引き継ぎつつ収集し，それを整理して利用しやすい形にまとめて公開していくことを活動の主目的としています．<br />
-          以前は過去問がサークルやクラス内のみで共有され，人によっては閲覧できない環境がありましたが，現在は京大wikiの存在で過去問を全学で学年間で共有することが可能となっています．
-          また学内の情報についても，明文化されず口伝えされること，SNSで書かれてもすぐに散逸することをまとめて文章にするのは重要だと考えて記事を作成しています．
-          本大学の学生文化が引き継いできたものの重さに対して4年間は短く，断絶がたやすく起きてしまうため，それを文章にして残して置くことは大切だと考えています．
-        </v-col>
-        <v-col cols='12'>
-          <h2>経緯</h2>
-          <div>2014年</div>理学部生が個人的に過去問を収集、過去問を見せた相手から試験問題を回収 <br />
-          <div>2018年</div> 過去問データベースの引き継ぎ 過去問をGoogleサイトでネット上で広く公開し広く過去問を募るように <br />
-          <div>2020年</div> 各自治会との連携を開始　現サイトに移行 過去問の閲覧を京大生に限定<br />
+          <h3>京大wiki運営について</h3>
+          学生有志がごく少数でやってます．利益は特にありません
+          <div>
+            <h4>活動目的</h4>
+            学生の立場で，学内の資料を引き継ぎつつ収集し公開していくことを活動の主目的としています．
+            全学で学年を越えて過去問を共有し誰でも閲覧できるようにしています．
+            また明文化されず口伝えされることやSNSで書かれていることが散逸しないように記事を作成しています
+          </div>
+          <div>
+            <h4>活動経緯</h4>
+            <h5>2014年</h5> ある学生が過去問を収集，過去問を見せた相手から問題を回収 <br />
+            <h5>2018年</h5> 過去問をGoogleサイトでネット上に公開し過去問を募るように <br />
+            <h5>2020年</h5> 各自治会との連携を開始，現サイトに移行して過去問の閲覧を京大生に限定<br />
+          </div>
+          <div>
+            <h4>運営参加希望の方へ</h4>
+
+          </div>
         </v-col>
       </v-row>
-    </v-container>
-    <v-container>
       <v-row>
         <v-col cols='12'>
-          <h2>関連サイト</h2>
+          <h3>関連サイト</h3>
           <ul>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
+            <li>
+              KU1025 - 物工過去問サイト
+              <v-btn icon href="https://1025-kuexam.netlify.app/">
+                <v-icon>mdi-link</v-icon>
+              </v-btn>
+            </li>
+            <li>
+              KUEE13 - 電電過去問サイト
+              <v-btn icon href="https://kuee.netlify.app/">
+                <v-icon>mdi-link</v-icon>
+              </v-btn>
+            </li>
+            <li>
+              理学部学生自治会サイト
+              <v-btn icon href="https://web.sci-ku.info/home">
+                <v-icon>mdi-link</v-icon>
+              </v-btn>
+            </li>
+
           </ul>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols='12'>
-          <h3>京大wikiにもっと携わりたい方へ</h3>
-          現在学生ごく数名で運営しています．
-          興味ある方はメール，DMにて連絡をください．
-          新機能開発企画や記事作成企画の持ち込みなども歓迎します．
         </v-col>
       </v-row>
       <v-row>
         <v-col cols='12'>
           <h3>お問い合わせ</h3>
-          メールおよびTwitterのDMにて対応致します<br />
-          京大wikiに関する質問，サイト不具合の報告，機能改善のリクエスト，記事掲載依頼，誤字指摘，その他，お気軽にご連絡ください．
+          メールかTwitterのDMにてご連絡ください．<br>
+          京大wikiに関する質問，サイト不具合の報告，機能改善のリクエスト，その他なんでもお気軽にどうぞ．
         </v-col>
       </v-row>
     </v-container>
   </v-main>
   <v-footer padless>
     <v-container>
-      <v-avatar size="26" class='my-1 mx-2'>
-        <img src="/kiwi.svg" alt="kiwi">
-      </v-avatar>
-      <span>京大wiki</span>
-      <v-btn icon href='/'>
-        <v-icon>mdi-link</v-icon>
-      </v-btn>
+      <div>
+        <v-avatar size="26" class='my-1 mx-2'>
+          <img class='kiwi-icon' src="/kiwi.svg" alt="kiwi">
+          <svg>
+          <use xlink:href="logo.svg" style="--color_fill: #000;"></use>
+        </svg>
+        </v-avatar>
+        <span>京大wiki</span>
+        <v-btn icon href='/'>
+          <v-icon>mdi-link</v-icon>
+        </v-btn>
+      </div>
       <div>
         <v-avatar size="26" class='my-1 mx-2'>
           <v-icon>mdi-twitter</v-icon>
@@ -199,13 +258,49 @@
   font-weight: 700;
 }
 
+.under {
+  text-decoration: underline;
+  /* vertical-align: text-bottom; */
+}
+
 div#kuwiki-is {
-  vertical-align: bottom;
+  vertical-align: text-bottom;
   font-family: "Noto Sans JP", sans-serif;
   font-size: 1.1rem;
 }
 
+dl>div {
+  margin: 10pt;
+}
+
+dd {
+  font-size: 0.8rem;
+}
+
 .gmail::after {
   content: '@gmail.com';
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+}
+
+h3 {
+  position: relative;
+  padding: 0.25em 0;
+  margin: 1em 0em 0.75em;
+}
+
+h3:after {
+  content: "";
+  display: block;
+  height: 4px;
+  background: -webkit-linear-gradient(to right, var(--v-primary-base), transparent);
+  background: linear-gradient(to right, var(--v-primary-base), var(--v-primary-base));
+}
+
+.kiwi-icon {
+  filter: invert(1) grayscale(100%);
 }
 </style>
