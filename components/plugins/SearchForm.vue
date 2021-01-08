93ccbde7c517913346a814329a88f257e0271bb6
kuwiki <siketaiscience@gmail.com>
2021-01-08 20:24:08 +0900

    検索機能改善
    lodash導入
    遅延しての検索
    参考
    https://jp.vuejs.org/v2/guide/computed.html#%E7%AE%97%E5%87%BA%E3%83%97%E3%83%AD%E3%83%91%E3%83%86%E3%82%A3-vs-%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89

--- components/plugins/SearchForm.vue
+++ components/plugins/SearchForm.vue
@@ -1,60 +1,80 @@
 <template>
-<v-app id='search-form'>
-  <v-row justify='center'>
-    <span id='searchform'>
-      <div class='ml-15 my-3 text-h5'>{{ title }}</div>
-      <v-text-field solo rounded prepend-inner-icon="mdi-magnify" autofocus
-       v-model="keyword"
-       v-on:input="searchItems">
-      </v-text-field>
-      <v-simple-table dense fixed-header>
-        <tbody>
-          <tr v-for="hitItem in hitItems">
-            <td>
-              <nuxt-link class='itemlink' :to="`${$route.path}/${hitItem.id}`">
-                <div>{{ hitItem.name }}</div>
-              </nuxt-link>
+<v-row justify='center'>
+  <v-col cols='12'>
+    <div class='my-3 text-center'>{{ title }}</div>
+  </v-col>
+  <v-col cols='12'>
+    <v-text-field v-model="searchkey" prepend-inner-icon="mdi-magnify" solo rounded autofocus>
+    </v-text-field>
+    <!-- <p> {{message}} </p> -->
+    <v-progress-circular v-if="message == '入力中' " :size="50" color="primary" indeterminate></v-progress-circular>
+    <v-simple-table dense fixed-header>
+      <tbody>
+        <tr v-for="searchresult in searchresults" :key="searchresult.id">
+            <td v-if="displayTable">
+              <NuxtLink no-prefetch class='itemlink black--text' :to="`${$route.path}/${searchresult.id}`">
+                {{ searchresult.name }}
+              </NuxtLink>
             </td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-    </span>
-  </v-row>
-</v-app>
+        </tr>
+      </tbody>
+    </v-simple-table>
+  </v-col>
+</v-row>
 </template>
 <script>
 import axios from 'axios';
+import _ from 'lodash';
 export default {
   data: () => ({
-    keyword: '',
-    hitItems: [],
+    searchkey: '',
+    searchresults: [],
+    displayTable: true,
+    message: '',
   }),
   props: {
     items: String,
     title: String,
   },
+  watch: {
+    searchkey: function(newkeyword, oldkeyword) {
+      this.message = '入力中'
+      this.searchKeyword()
+    },
+  },
+  created: function() {
+    this.searchKeyword = _.debounce(this.hitApi, 500)
+  },
   methods: {
-    searchItems() {
-      var SEARCH_API_URL = process.env.WIKI_API_URL + '/' + this.items + '/search/' + this.keyword
-      axios.get( SEARCH_API_URL ).then(res => {
-          this.hitItems = res.data
-        });
+    hitApi: function() {
+      var SEARCH_API_URL = process.env.WIKI_API_URL + '/' + this.items + '/search/' + this.searchkey
+      console.log(this.searchkey)
+      axios
+        .get(SEARCH_API_URL)
+        .then((res) => {
+          this.displayTable = true;
+          this.message ='検索結果'
+          this.searchresults = res.data;
+        })
+        .catch((err) => {
+          this.message = err;
+          this.displayTable = false;
+        })
     }
   }
 }
 </script>
 <style>
-span#searchform .v-text-field input {
+.v-text-field input {
   padding: 8px 10px 8px;
 }
 
-span#searchform {
+.v-text-field {
   width: 350px;
   margin: 20px 10px 20px;
 }
 
 .itemlink {
   text-decoration: none;
-  color: blue;
 }
 </style>
