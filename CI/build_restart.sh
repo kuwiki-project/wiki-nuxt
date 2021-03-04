c213b4f9c57b134d6dca982ef708c0db3fc9a18d
NishikyoKU <nishikyo_ku@LAPTOP-D9IN7IJ1.localdomain>
2021-03-04 21:55:25 +0900

    オートデプロイ用のシェルスクリプトを作成

---
+++ CI/build_restart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# git pullする
+sudo git pull origin master
+# 必要なパッケージをインストール
+sudo yarn
+# ビルド
+sudo yarn build
+# すでに動いているものをストップ
+ps --no-heading -C "sudo nohup yarn wiki-nuxt-start" -o pid | awk '{ print "kill " $0 }' | sudo sh
+# スタート
+sudo nohup yarn wiki-nuxt-start &
