66ebfe905675f766e89a14e0cef028226f6d0bf4
kuwiki <kuwiki99@gmail.com>
2021-04-15 13:50:19 +0900

    回収済みページにローディングマーク表示

--- components/docs/Collected.vue
+++ components/docs/Collected.vue
@@ -2,6 +2,14 @@
   <div>
     <v-card-title>2020後期回収済みリスト</v-card-title>
     <v-card-text>
+      <div class="my-3 text-center">
+        <v-progress-circular
+          v-if="loading === true"
+          color="primary"
+          indeterminate
+        />
+      </div>
+
       <li v-for="(collected_exam, key) in collected_exams" :key="key">
         {{ collected_exam.file_title }}
       </li>
@@ -12,6 +20,7 @@
 export default {
   data() {
     return {
+      loading: true,
       collected_exams: null
     }
   },
@@ -19,6 +28,7 @@ export default {
     this.collected_exams = await fetch(
       this.$config.COLLECTED_EXAMS_JSON_URL
     ).then((res) => res.json())
+    this.loading = false
   }
 }
 </script>
